18972d7727d7268c04e1c7b122d1ea039c560f11
tkrsss <rian_dias@estudante.sesisenai.org.br>
2024-05-29 08:03:51 -0300

    feito tela de login

--- projeto_eptran/script/script.js
+++ projeto_eptran/script/script.js
@@ -2,6 +2,8 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     event.preventDefault();
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+    
     console.log("Usuário:", username);
     console.log("Senha:", password);
+    
 });
