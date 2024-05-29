5c08e7e97c808db43eb423a72dd06eabfdb472cf
Max <max_prochnow@estudante.sesisenai.org.br>
2024-05-29 09:31:32 -0300

    Iniciado a pagina de curiosidade.

--- projeto_eptran/script/script2.js
+++ projeto_eptran/script/script2.js
@@ -1,19 +1,13 @@
-document.getElementById('showFactBtn').addEventListener('click', showFact);
+document.addEventListener('DOMContentLoaded', showFact);
 
 const facts = [
-    "Apenas 8% das pessoas conseguem dobrar a língua em forma de trevo.",
-    "O coração de um camarão está em sua cabeça.",
-    "A Terra é o único planeta que não tem nome de um deus.",
-    "Um raio pode atingir uma temperatura de 30.000 Kelvin (53.540 graus Fahrenheit).",
-    "As vacas têm melhores amigas e ficam estressadas quando são separadas.",
-    "O mel nunca estraga. Potes de mel encontrados em tumbas egípcias têm mais de 3000 anos e ainda são comestíveis.",
-    "Bananas são bagas, mas morangos não são.",
-    "Os flamingos são naturalmente brancos; eles ficam rosa por causa dos crustáceos na sua dieta."
+    "Seja bem-vindo ao nosso site dedicado ao universo do trânsito! Aqui, buscamos ser a sua fonte confiável de informações, dicas e soluções para tornar a sua experiência no trânsito mais segura, eficiente e agradável.",
+    "Navegue por nosso conteúdo diversificado, desenvolvido por especialistas e entusiastas do trânsito, e descubra recursos úteis para aprimorar seu conhecimento, aumentar sua segurança e contribuir para um tráfego mais harmonioso.",
+   
 ];
 
 function showFact() {
     const factElement = document.getElementById('fact');
     const randomIndex = Math.floor(Math.random() * facts.length);
     factElement.innerText = facts[randomIndex];
-    factElement.style.display = 'block';
 }
